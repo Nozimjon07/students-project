8fc9f52f5c18dbc0ec68258d37eee1796ce5685d
Ahmadov Ozodbek <ahmadovozodbek80@gmail.com>
2024-11-29 00:08:10 +0530

    Second devoloper complete!(+.+)

--- students/migrations/0001_initial.py
+++ students/migrations/0001_initial.py
@@ -18,8 +18,7 @@ class Migration(migrations.Migration):
                 ('first_name', models.CharField(max_length=50)),
                 ('last_name', models.CharField(max_length=50)),
                 ('email', models.EmailField(max_length=254, unique=True)),
-                ('date_of_birth', models.DateField()),
-                ('enrollment_date', models.DateField(auto_now_add=True)),
+                ('age', models.IntegerField()),
             ],
         ),
     ]
--- students/views.py
+++ students/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Student
+# Student.objects.all().delete()
 
-# Create your views here.
+def student_list(request):
+    first_name = request.GET.get("first_name")
+    last_name = request.GET.get("last_name")
+    email = request.GET.get("email")
+    age = request.GET.get("age")
+    if first_name is not None and last_name is not None and email is not None and age is not None:
+        Student.objects.create(
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            age=age,
+        )
+    students = Student.objects.all()
+    return render(request, "students/student_list.html", {'students': students})
